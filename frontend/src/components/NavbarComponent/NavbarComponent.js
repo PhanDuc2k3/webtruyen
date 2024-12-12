ecba83a3299744199397c27b80729342bf173399
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2024-12-12 16:54:35 +0700

    fix navbar

--- frontend/src/components/NavbarComponent/NavbarComponent.js
+++ frontend/src/components/NavbarComponent/NavbarComponent.js
@@ -5,43 +5,51 @@ const NavbarComponent = () => {
     return (
         <div>
             <div class="navbar-all">
-    <ul class="navbar">
-        <li>
-            COMICS
-            <ul class="dropdown">
-                <li>TOP MANHWA</li>
-                <li>TOP MANHUA</li>
-                <li>TOP MANGA</li>
-            </ul>
-        </li>
-        <li >
-            NEW COMICS                
+              <ul class="navbar">
+                    <li>
+                     COMICS
+                        <ul class="dropdown">
+                            <li>TOP MANHWA</li>
+                            <li>TOP MANHUA</li>
+                            <li>TOP MANGA</li>
+                        </ul>
+                    </li>
+                    <li >
+                    NEW COMICS                
 
-        </li>
-        <li>
-            HOT MANHWA
-            <ul class="dropdown">
-                <li>Company</li>
-                <li>Team</li>
-                <li>Careers</li>
-            </ul>
-        </li>
-        <li>
-            MORE
-            <ul class="dropdown">
-                <li>Email</li>
-                <li>Phone</li>
-            </ul>
-        </li>
-        <li>
-            RECOMNEND
-        </li>
-        <li>
-            GENRES
-        </li>
-    </ul>
-</div>
-
+                    </li>
+                    <li>
+                    HOT MANHWA
+                        <ul class="dropdown">
+                            <li>Company</li>
+                            <li>Team</li>
+                            <li>Careers</li>
+                        </ul>
+                    </li>
+                    <li>
+                    MORE
+                        <ul class="dropdown">
+                            <li>Email</li>
+                            <li>Phone</li>
+                        </ul>
+                    </li>
+                    <li>
+                    RECOMNEND
+                    </li>
+                    <li>
+                    GENRES
+                    </li>
+                </ul>
+                <div className="navbar-button-login-register">
+                    <button>
+                        Sign in
+                    </button>    
+                    <button>
+                        Sign Up
+                    </button> 
+                </div>
+        </div>
+        
         </div>
     )
 }
