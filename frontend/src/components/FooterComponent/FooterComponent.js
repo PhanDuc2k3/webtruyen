e67573bc0b83420b18e4f46cd31f3605c4e69113
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2024-12-12 17:19:04 +0700

    done footer-component

--- frontend/src/components/FooterComponent/FooterComponent.js
+++ frontend/src/components/FooterComponent/FooterComponent.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import "./FooterComponent.scss"
 const FooterComponent = () => {
     return (
         <div className="FooterContainer">
-            <p>Footer</p>
+            <ul className="footer-list">
+                <li><a href="#home">HOME</a></li>
+                <li><a href="#contact">CONTACT</a></li>
+                <li><a href="#about-us">ABOUT US</a></li>
+                <li><a href="#cookie-policy">COOKIE POLICY</a></li>
+                <li><a href="#dmca">DMCA</a></li>
+                <li><a href="#privacy-policy">PRIVACY POLICY</a></li>
+            </ul>
+
+
+            <div>
+                <p style={{
+                    color: "gray",
+                    fontSize: "14px",
+                    display: "flex",
+                    justifyContent: "center",
+                }}>
+                    © 2024 TOP MANHUA Inc. All rights reserved
+                </p>
+            </div>
         </div>
     );
 };
